e452cf9309fb49d544b2f87197335716359f2d10
Leonardo Sales <lsales@globosat.com.br>
2015-08-29 16:52:11 -0300

    Criando controller welcome

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,9 @@
 Blog::Application.routes.draw do
+  get "contato/index"
+
+  get "welcome/index"
+
+  #get "welcome/index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
