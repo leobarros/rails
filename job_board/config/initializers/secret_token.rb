2f8f911ad35c232322b8104c29915b8fec6a8243
Leonardo Sales <lsales@globosat.com.br>
2015-08-29 16:54:33 -0300

    Projeto Classificado de empregos

---
+++ job_board/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobBoard::Application.config.secret_token = '363c325932614312bb182a3101e645d244e51e2b6af07181de25c6c1eb622b138764c18551ed265867afb5c8d205c1875aaf3df430854c0435d00c56c4dc2532'
